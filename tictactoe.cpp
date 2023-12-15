ec66e0838bb9cfbba9ec52eafb26ba2cdb73ec6a
phavanee <147310184+phavanee@users.noreply.github.com>
2023-12-15 15:55:06 +0800

    added boolean function to check winner it DOES NOT work i'll fix it after my nap mimimimimimi

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,73 +1,72 @@
-// tic tac toe project
-/* phase 1:
-    create a function that prints out the board
-    add a condition: to print out the board as long as there is no win yet
-    allow it to take input from 2 users*/
-
 #include <iostream>
 #include <conio.h>
 
 using namespace std;
 
 void displayBoard();
-int playerOne();
-int playerTwo();
-int modifyBoard(int gamePiece);
+int playerMove();
+// int playerTwo();
+int modifyBoard(int gamePiece, char currentPlay);
+bool checkWin();
 
 char bp[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
 int main(){
-    int a = 0;
+    char currentPlayer = 'X';
     cout << "Welcome to this tic tac toe game!" << endl;
     do{
         displayBoard();
-        modifyBoard(playerOne());
-        displayBoard();
-        modifyBoard(playerTwo());
-        ++a;
-    } while(a<4);
+        modifyBoard(playerMove(), currentPlayer);
+        checkWin();
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        // displayBoard();
+        // modifyBoard(playerTwo());
+    } while(checkWin==0);
+
     getch();
     return 0;
 }
 
 void displayBoard(){
-    for(int i=0;i<10;i++){
-        cout << bp[i] << "   ";
-
-        if((i+1)%3==0){
-            cout << "\n---------\n";
-        }
-    }
+    cout << bp[0] << " | " << bp[1] << " | " << bp[2];
+    cout << "\n---------\n";
+    cout << bp[3] << " | " << bp[4] << " | " << bp[5];
+    cout << "\n---------\n";
+    cout << bp[6] << " | " << bp[7] << " | " << bp[8];
+     
 }
 
-int playerOne(){
+int playerMove(){
     int p1;
-    cout << "\nWhere would you like to place your piece (x)? ";
+    cout << "\nWhere would you like to place your piece? ";
     cin >> p1;
     cout << "You have placed your piece on " << p1 << "\n";
     return p1;
 }
 
-int playerTwo(){
-    int p2;
-    cout << "\nWhere would you like to place your piece (o)? ";
-    cin >> p2;
-    cout << "You have placed your piece on " << p2 << "\n";
-    return p2;
-}
+// int playerTwo(){
+//     int p2;
+//     cout << "\nWhere would you like to place your piece (O)? ";
+//     cin >> p2;
+//     cout << "You have placed your piece on " << p2 << "\n";
+//     return p2;
+// }
 
 // create a function that takes user input
 // and modifies the tictactoe board
-int modifyBoard(int gamePiece){
-    static int turn = 0;
-    ++turn;
-
-    if(turn%2!=0){
-        bp[gamePiece-1]='x';
-    }
-    else{
-        bp[gamePiece-1]='o';
-    }
+int modifyBoard(int gamePiece, char currentPlay){
+    
+    bp[gamePiece-1] = currentPlay;
 
     return 0;
+}
+
+bool checkWin(){
+    if(bp[0]==bp[1]==bp[2] || bp[3]==bp[4]==bp[5] || bp[6]==bp[7]==bp[8] || 
+    bp[0]==bp[3]==bp[6] || bp[1]==bp[4]==bp[7] || bp[2]==bp[5]==bp[8] ||
+    bp[0] == bp[4] == bp[8] || bp[2] == bp[4] == bp[6]){
+        cout << "We have a winner! " << endl;
+        return true;
+    }
+    return false;
 }
